2bbff50711c7a2c1b287d398282259c642bb9d2b
fktona <adetona.fktona@gmail.com>
2024-05-31 02:49:23 +0100

    [completed]

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -6,13 +6,13 @@ export default function Hero() {
   return (
     <main className='relative max-w-[1440px] mx-auto  font-ppPlus lg:px-[102px] lg:pb-[350px] px-[21px] mt-12   '>
         <Nav/>
-        <div className='lg:max-w-[50%]  space-y-5 mt-12  z-50 relative'>
+        <div className='lg:max-w-[50%]  space-y-8 mt-12  z-50 relative'>
         <h1 className='font-bold lg:text-[88.7px] text-[45px] '>$PEPEPEDRO</h1>
         <p className='lg:text-[34px] md:text-[24px] text-[17.5px]'>
         Lorem ipsum dolor sit amet, consectetuer 
         adipiscing elit. Aenean commodo ligula eget dolor
         </p>
-        <button className='bg-[#c3564d] hover:bg-[#c3564d]/75 arial mt-4 md:text-[24px] text-[17.5px] px-5 py-3 lg:px-[40px] py-[18px]
+        <button className='bg-[#c3564d] hover:bg-[#c3564d]/75 arial  md:text-[24px] text-[17.5px] px-5 py-3 lg:px-[40px] py-[18px]
           text-white rounded-[42px]'>Buy on Dexscreener</button>
         </div>
         <div className='lg:absolute bottom-0  lg:block   lg:max-w-[65%] right-0 max-h-[80%] mt-12 relative'>
