90d67fa25a6cfbdb78c70c158fd60dc16cbea1c0
fktona <adetona.fktona@gmail.com>
2024-05-31 03:09:59 +0100

    [completed]

--- app/components/play.jsx
+++ app/components/play.jsx
@@ -38,7 +38,21 @@ export default function Play() {
           loop={true}
         //   controls={true}
         />
+         {/* <div className='absolute flex  items-center justify-center rounded-[145px] min-w-[70%] max-h-[200px]
+         lg:max-h-[622px] lg:min-h-[622px] lg:rounded-[311px]
+          lg:h-[40vh] h-[60vh]  bg-black/10 z-10 w-full h-full'>
+            <button className='bg-white/40 p-8 rounded-full'>
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-12">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+</svg>
+</button>
+
+
+</div> */}
       </div>
+
+     
     </div>
   );
 }
