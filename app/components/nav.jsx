66afaab62545dc857d91057fc0f1a4919895fbb8
fktona <113286902+fktona@users.noreply.github.com>
2024-05-31 13:00:53 +0100

    Update nav.jsx

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -4,7 +4,8 @@ export default function Nav() {
   return (
     <nav className='flex items-center mt-4  justify-between lg:py-[51px] '>
         <h2 className='lg:text-[44px] text-[28px]  leading-[51px]'>$Pedro</h2>
-        <button className='lg:text-[24px] arial bg-[#1c0d0e] hover:bg-[#1c0d0e]/75 text-white rounded-[42px] leading-[27.6px] lg:px-10 px-6 lg:py-5 py-2 '><a href="https://t.me/PedroPeSOL" target="_blank"></a>Join Telegram</button>
+        <button className='lg:text-[24px] arial bg-[#1c0d0e] hover:bg-[#1c0d0e]/75 text-white rounded-[42px] leading-[27.6px] lg:px-10 px-6 lg:py-5 py-2 '><a href="https://t.me/PedroPeSOL" target="_blank">Join Telegram</a>
+      </button>
     </nav>
   )
 }
