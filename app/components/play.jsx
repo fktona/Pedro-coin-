320de720305e9a39d0949ac96c2375edb3f7cdbb
fktona <113286902+fktona@users.noreply.github.com>
2024-05-31 12:42:45 +0100

    Update play.jsx

--- app/components/play.jsx
+++ app/components/play.jsx
@@ -26,8 +26,8 @@ export default function Play() {
   return (
     <div className='mx-auto relative max-w-[1440px] w-full relative'>
         <div className="relative w-full lg:px-[100px] px-8 flex items-center justify-between text-[30px] lg:text-[50px] mt-8">
-        <p className="font-ppPlus font-bold flex items-center gap-4">Click <br/> to play <Image alt="arrow" src="/arrow.svg" width={50} height={50} /></p>
-       <p className="font-ppPlus font-bold flex items-center hidden lg:block gap-4"><Image alt="arrow" src="/arrow2.svg" width={50} height={50} />
+        <p className="font-ppPlus font-bold flex items-center gap-4">Click <br/> to play <Image alt="arrow" src="/arrow.svg" width={80} height={80} /></p>
+       <p className="font-ppPlus font-bold flex items-center w-full hidden lg:block gap-4"><Image alt="arrow" src="/arrow2.svg" width={80} height={80} />
    Click <br/>HERE 
                </p>
         </div>
