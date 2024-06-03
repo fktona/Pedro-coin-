31ae4e52670bdced2ddad045d9a11c1525a0b68c
fktona <113286902+fktona@users.noreply.github.com>
2024-06-03 18:01:59 +0100

    Update abt.jsx

--- app/components/abt.jsx
+++ app/components/abt.jsx
@@ -15,7 +15,7 @@ Abt() {
             <Image alt='star' src={'/star.svg'} width={9.5} height={9.5} className='relative lg:w-[17px] -bottom-[60px] aspect-square'/>
           </div>
             <h1 className='lg:text-[96px] font-bold text-white text-center text-[40px] lg:leading-[111px]'>
-            ABOUT $PEPEPEDRO
+            About $pedro
             </h1>
             <p className='lg:text-[37px] text-[17.5px] font-bold text-white  text-center'>
 
@@ -28,7 +28,7 @@ Abt() {
                 bg-[#FFF9D9] hover:bg-[#FFF9D9]/75'>Dextool</button>
                 <button className='rounded-[42px] lg:py-[18px] px-[24px] py-3 lg:px-[40px] lg:text-[24px] 
                 text-[17.5px]
-                 bg-[#FFDA19] hover:bg-[#FFDA19]/75'>Dexscreener</button>
+                 bg-[#FFDA19] hover:bg-[#FFDA19]/75'><a href="https://dexscreener.com/solana/6VBp6ZB8qiJE4ciq3mEnkbLHegixFNAv8NAqAPEoQcQs" target="_blank">Dexscreener</a></button>
             </div>
 
         </div>
